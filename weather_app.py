5ea59065b2ae6a61aa33307fc57c1d2c0babd15b
Şirəli Əlihümmətov <shiralialihummatov@gmail.com>
2021-05-28 01:22:21 +0400

    Weather App

--- weather_app.py
+++ weather_app.py
@@ -1,51 +1,90 @@
 from tkinter import *
 import requests
 import time
+from datetime import datetime
 
 
-def get_weather(window):
-    city = entry_city.get()
-    api = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid=a4268b8e63c13c18c3dfad2472682603"
-    json_data = requests.get(api).json()
-    condition = json_data['weather'][0]['main']
-    temp = int(json_data['main']['temp'] - 273.15)
-    min_temp = int(json_data['main']['temp_min'] - 273.15)
-    max_temp = int(json_data['main']['temp_max'] - 273.15)
-    pressure = json_data['main']['pressure']
-    humidity = json_data['main']['humidity']
-    wind = json_data['wind']['speed']
-    sunrise = time.strftime('%I:%M:%S', time.gmtime(json_data['sys']['sunrise']-21600))
-    sunset = time.strftime('%I:%M:%S', time.gmtime(json_data['sys']['sunset']-21600))
+class App:
+    def __init__(self):
+        self.window = Tk()
+        self.window.geometry('1080x600+200+100')
+        self.window.title('Weather App')
 
-    final_info = f'{condition}\n{str(temp)}°C'
-    final_data = f'\nMax Temp: {str(max_temp)}\n' \
-                 f'Min Temp: {str(min_temp)}\n' \
-                 f'Pressure: {str(pressure)}\n' \
-                 f'Humidity: {str(humidity)}\n' \
-                 f'Wind speed: {str(wind)}\n' \
-                 f'Sunrise: {sunrise}\n' \
-                 f'Sunset: {sunset}'
+        self.window.resizable(width=False, height=False)
+        self.window.configure(bg='#2b2e2c')
 
-    label_1.config(text=final_info)
-    label_2.config(text=final_data)
+        self.window.iconphoto(False, PhotoImage(file='icon_photo.png'))
+
+        self.font_1 = ('poppins', 15, 'bold')
+        self.font_2 = ('poppins', 24, 'bold')
+        self.font_3 = ('poppins', 35, 'bold')
+
+        self.gui_components()
+
+        self.window.mainloop()
+
+    def gui_components(self):
+        self.label = Label(self.window, font='poppins 35 bold', bg='#1b1c1b', fg='yellow', text='Welcome to Weather app')
+        self.label.place(relx=0, rely=0, relheight=0.12, relwidth=1)
+
+        self.entry_city = Entry(self.window, justify=CENTER, font=self.font_1)
+        self.entry_city.place(relx=0.25, rely=0.2, relheight=0.05, relwidth=0.5)
+        self.entry_city.focus()
+        self.entry_city.bind('<Return>', self.get_weather)
+
+        self.label_1 = Label(self.window, font=self.font_2, bg='#2b2e2c', fg='white', borderwidth=1, relief="solid")
+        self.label_1.place(relx=0.04, rely=0.6, relheight=0.24, relwidth=0.28)
+
+        self.label_2 = Label(self.window, font=self.font_2, bg='#2b2e2c', fg='white', borderwidth=1, relief="solid")
+        self.label_2.place(relx=0.36, rely=0.6, relheight=0.24, relwidth=0.28)
+
+        self.label_3 = Label(self.window, font=self.font_2, bg='#2b2e2c', fg='white', borderwidth=1, relief="solid")
+        self.label_3.place(relx=0.68, rely=0.6, relheight=0.24, relwidth=0.28)
+
+        self.label_4 = Label(self.window, font=self.font_3, bg='#2b2e2c', fg='white')
+        self.label_4.place(relx=0.45, rely=0.32)
+
+        self.label_5 = Label(self.window, font=self.font_1, bg='#2b2e2c', fg='white')
+        self.label_5.place(relx=0.46, rely=0.42)
 
 
-window = Tk()
-window.geometry('600x500')
-window.title('Weather App')
+    def get_weather(self, window):
+        city = self.entry_city.get()
+        api = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid=a4268b8e63c13c18c3dfad2472682603"
+        json_data = requests.get(api).json()
+        condition = json_data['weather'][0]['main']
+        condition_description = json_data['weather'][0]['description']
+        icon_address = json_data['weather'][0]['icon']
+        temp = int(json_data['main']['temp'] - 273.15)
+        min_temp = int(json_data['main']['temp_min'] - 273.15)
+        max_temp = int(json_data['main']['temp_max'] - 273.15)
+        pressure = json_data['main']['pressure']
+        humidity = json_data['main']['humidity']
+        wind = json_data['wind']['speed']
+        sunrise = time.strftime('%I:%M:%S', time.gmtime(json_data['sys']['sunrise'] - 21600))
+        sunset = time.strftime('%I:%M:%S', time.gmtime(json_data['sys']['sunset'] - 21600))
+        today = datetime.date(datetime.now())
 
-font_1 = ('poppins', 15, 'bold')
-font_2 = ('poppins', 25, 'bold')
+        label_hour_info = f'Today: {str(today)}\n' \
+                          f'Sunrise: {str(sunrise)}\n' \
+                          f'Sunset: {str(sunset)}'
 
-entry_city = Entry(window, justify=CENTER, font=font_1)
-entry_city.pack(pady=20, ipadx= 150)
-entry_city.focus()
-entry_city.bind('<Return>', get_weather)
+        label_temp_info = f'Temperature: {str(temp)}°C\n' \
+                          f'Max Temp: {str(max_temp)}°C\n' \
+                          f'Min Temp: {str(min_temp)}°C'
 
-label_1 = Label(window, font=font_1)
-label_1.pack()
-label_2 = Label(window, font=font_2)
-label_2.pack()
+        label_addition_info = f'Pressure: {str(pressure)}\n' \
+                              f'Wind speed: {str(wind)}\n' \
+                              f'Humidity: {str(humidity)}'
 
 
-window.mainloop()
+
+        self.label_1.config(text=label_hour_info)
+        self.label_2.config(text=label_temp_info)
+        self.label_3.config(text=label_addition_info)
+        self.label_4.config(text=condition)
+        self.label_5.config(text=condition_description)
+
+
+if __name__ == '__main__':
+    app = App()
